6af791e48711ddbb01a293fa1ddcf3e45547154a
Mark Ringor <mcringor@affinitycorp.net>
2019-10-24 16:22:32 +0800

    initial commit

---
+++ build/precache-manifest.78436c437455a16a2db74be52c478fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca0d7490a55b4b0eb5192dca6a639f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7bf9bf93927e538646e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "07f9361d50ce0554af0d",
+    "url": "/static/js/2.b5985af0.chunk.js"
+  },
+  {
+    "revision": "e7bf9bf93927e538646e",
+    "url": "/static/js/main.b0b4d09e.chunk.js"
+  },
+  {
+    "revision": "867559b179ea8c7af62a",
+    "url": "/static/js/runtime-main.6a03bc6f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
